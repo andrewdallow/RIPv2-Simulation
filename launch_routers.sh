642382f7b85e0047deb21f1376f7f962d5a88917
dthg <captainhawkward@gmail.com>
2015-04-17 13:55:52 +1200

    added shell scripts to kill and launch the routers.

---
+++ launch_routers.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ROUTERS="1 2 3" #4 5 6 7"
+
+for rout_num in $ROUTERS
+do
+	python3 Router.py "router_$rout_num.txt" & pids="${pids-} $!"
+done
+echo $pids
+
+echo $pids > pid_list.txt
